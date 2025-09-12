5bfffee8d9ec965dd4b60f691dfaa291bc513eba
lu-antony <lu-antony@yahoo.com.tw>
2025-09-12 09:46:08 +0800

    加入專案檔案。

---
+++ MiniXafApi.WebApi/BusinessObjects/Employee.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace MiniXafApi.WebApi.BusinessObjects
+{
+    public class Employee : BaseObject
+    {
+        public Employee(Session session) : base(session) { }
+
+
+        private string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set { SetPropertyValue<string>(nameof(Name), ref _Name, value); }
+        }
+
+
+    }
+}
